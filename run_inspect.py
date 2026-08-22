575e29302561e4bfb4da82d875574d5ada122f25
hisamatsurie <hisamatsurie@gmail.com>
2026-08-22 14:27:26 +0900

    Delete run_inspect.py

--- run_inspect.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Test script to run inspect_html and capture output
-"""
-import subprocess
-import sys
-
-result = subprocess.run([sys.executable, "inspect_html.py"], capture_output=False, text=True)
-sys.exit(result.returncode)
